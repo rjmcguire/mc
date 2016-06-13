742586b854d8bea156f9ed5e6be2f3de2d8c8da3
Andrew Borodin <aborodin@vmail.ru>
2016-06-13 10:16:45 +0300

    Refactoring of widget drawing to avoid multiple widget redraw.
    Send MSG_DRAW message immediately after MSG_FOCUS/MSG_UNFOCUS.
    Thus, the MSG_DRAW message handler is the only place where widget
    should be drawn. Widget should not draw itself in other message
    handlers.
    
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- lib/widget/widget-common.c
+++ lib/widget/widget-common.c
@@ -257,6 +257,7 @@ widget_set_state (Widget * w, widget_state_t state, gboolean enable)
             ret = send_message (w, NULL, msg, 0, NULL);
             if (ret == MSG_HANDLED)
             {
+                send_message (w, NULL, MSG_DRAW, 0, NULL);
                 /* Notify owner that focus was moved from one widget to another */
                 send_message (w->owner, w, MSG_NOTIFY, (int) msg, NULL);
             }
